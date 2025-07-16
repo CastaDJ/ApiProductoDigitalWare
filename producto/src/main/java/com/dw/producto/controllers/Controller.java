3cd00406e7e5f7f8a3e69c43921f9e1fbb3624bd
Juan David Castañeda <jcastanedaaguirre@gmail.com>
2025-07-15 22:05:15 -0500

    Se configura los endPoints del controlador

--- producto/src/main/java/com/dw/producto/controllers/Controller.java
+++ producto/src/main/java/com/dw/producto/controllers/Controller.java
@@ -1,10 +1,74 @@
 package com.dw.producto.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dw.producto.entity.Producto;
+import com.dw.producto.services.ProductoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @RestController
 @RequestMapping("/api/producto")
 public class Controller {
 
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Producto>> getAllProduct() {
+        try {
+            List<Producto> productos = productoService.getAllProductos();
+            return ResponseEntity.ok(productos);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build();
+        }
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Producto> getProductById(@PathVariable Integer id) {
+        try {
+            Producto producto = productoService.getProductoById(id);
+            if (producto != null) {
+                return ResponseEntity.ok(producto);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build();
+        }
+    }
+
+   @PostMapping("/create")
+    public ResponseEntity<Producto> createProduct(@RequestBody Producto producto) {
+        try {
+            Producto createdProducto = productoService.createProducto(producto);
+            return ResponseEntity.status(201).body(createdProducto);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Producto> updateProduct(@PathVariable Integer id, @RequestBody Producto producto) {
+        try {
+            Producto updatedProducto = productoService.updateProducto(id, producto);
+            if (updatedProducto != null) {
+                return ResponseEntity.ok(updatedProducto);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build();
+        }
+    }
 }
